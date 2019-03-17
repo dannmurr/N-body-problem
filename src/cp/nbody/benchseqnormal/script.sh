aa0f8fecb9582c115fb4b91be17e9b9dec16bca5
abbenteprizer <abbenteprizer@gmail.com>
2019-03-17 18:53:00 +0100

    no snobdy

--- src/cp/nbody/benchseqnormal/script.sh
+++ src/cp/nbody/benchseqnormal/script.sh
@@ -1,3 +1,6 @@
-java snbody 120 50000 > data/sbench120.dat
-java snbody 180 50000 > data/sbench180.dat
-java snbody 240 50000 > data/sbench240.dat
+#!/bin/bash
+javac Nbody.java Point.java
+
+java Nbody 120 50000 > data/sbench120.dat
+java Nbody 180 50000 > data/sbench180.dat
+java Nbody 240 50000 > data/sbench240.dat
