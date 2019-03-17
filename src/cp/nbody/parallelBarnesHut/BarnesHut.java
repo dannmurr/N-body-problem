6f8aa49bfd032e1bb2f6dbac12825e52b595ad4e
dannmurr <daniel-muresu@hotmail.com>
2019-03-17 18:29:16 +0100

    added parallel Barnes hut implementation

---
+++ src/cp/nbody/parallelBarnesHut/BarnesHut.java
@@ -0,0 +1,123 @@
+package cp.nbody.parallelBarnesHut;
+
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class BarnesHut {
+
+    Point[] points;
+    double G = 6.67e-11;
+    double DT = 0.1;
+
+    int gnumBodies = 120;
+    int numSteps = 275000;
+    int numWorkers = 3;
+    double maxlength;
+    double theta = 0.8;
+
+    CyclicBarrier barrier;
+
+    public BarnesHut() {
+
+        double massOfBodies = 10;
+
+        maxlength = points[gnumBodies - 1].posX + 7;
+
+        points = new Point[gnumBodies];
+
+        barrier = new CyclicBarrier(numWorkers);
+
+        //INITIALIZE POINTS
+        Random r = new Random();
+        for (int i = 0; i < gnumBodies; i++) {
+            points[i] = new Point((10 * (i % (int) Math.sqrt(gnumBodies))) + r.nextDouble() * 7,
+                    10 * (i / (int) Math.sqrt(gnumBodies)) + r.nextDouble() * 7, 0, 0, 0, 0, massOfBodies, numWorkers);
+        }
+
+    }
+
+    public void barrier(int w) {
+        try {
+            barrier.await();
+        } catch (InterruptedException ex) {
+        } catch (BrokenBarrierException ex) {
+            Logger.getLogger(BarnesHut.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public static class Worker extends Thread {
+
+        int id;
+        BarnesHut work;
+
+        public Worker(int w, BarnesHut work) {
+            this.id = w;
+            this.work = work;
+        }
+
+        @Override
+        public void run() {
+
+            long start = 0, end = 0;
+
+            if (id == 0) {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("body " + i + " at " + work.points[i].posX);
+                }
+                start = System.nanoTime();
+            }
+
+            for (int i = 0; i < work.numSteps; i++) {
+                Quad q = new Quad(0, 0, work.maxlength);
+                BHTree tree = new BHTree(q, work.theta);
+
+                //create tree
+                for (int j = 0; j < work.gnumBodies; j++) {
+                    if (work.points[j].inQuad(q)) {
+                        tree.insertPoint(work.points[j]);
+                    }
+                }
+
+                //calculate forces for assigned points
+                for (int j = id; j < work.gnumBodies; j += work.numWorkers) {
+                    tree.calculateForce(work.points[j], id);
+                }
+
+                //wait for other workers to finish
+                work.barrier(id);
+
+                //move the points according to the forces
+                for (int j = id; j < work.gnumBodies; j += work.numWorkers) {
+                    work.points[j].movePoint();
+                }
+                work.barrier(id);
+
+            }
+            if (id == 0) {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("body " + i + " at " + work.points[i].posX);
+                }
+                end = System.nanoTime() - start;
+                System.out.println("total execution time: " + end * Math.pow(10, -9) + " seconds");
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        BarnesHut simulation = new BarnesHut();
+
+        for (int i = 0; i < simulation.numWorkers; i++) {
+            Worker worker = new Worker(i, simulation);
+            worker.start();
+        }
+
+    }
+
+}
