c6f05307b94cd7f49f8869945e26684873983009
abbenteprizer <abbenteprizer@gmail.com>
2019-03-17 19:46:02 +0100

    changed in pnormal hut

--- src/cp/nbody/benchparnormal/script.sh
+++ src/cp/nbody/benchparnormal/script.sh
@@ -5,36 +5,12 @@ java ParallelNbody 120 50000 1  >  data/bench120.dat
 java ParallelNbody 120 50000 2  >>  data/bench120.dat
 java ParallelNbody 120 50000 3  >>  data/bench120.dat
 java ParallelNbody 120 50000 4  >>  data/bench120.dat
-java ParallelNbody 120 50000 5  >>  data/bench120.dat
-java ParallelNbody 120 50000 6  >>  data/bench120.dat
-java ParallelNbody 120 50000 7  >>  data/bench120.dat
-java ParallelNbody 120 50000 8  >>  data/bench120.dat
-java ParallelNbody 120 50000 9  >>  data/bench120.dat
-java ParallelNbody 120 50000 10  >>  data/bench120.dat
-java ParallelNbody 120 50000 11  >>  data/bench120.dat
-java ParallelNbody 120 50000 12  >>  data/bench120.dat
-java ParallelNbody 120 50000 13  >>  data/bench120.dat
-java ParallelNbody 120 50000 14  >>  data/bench120.dat
-java ParallelNbody 120 50000 15  >>  data/bench120.dat
-java ParallelNbody 120 50000 16  >>  data/bench120.dat
 
 
 java ParallelNbody 180 50000 1  >  data/bench180.dat
 java ParallelNbody 180 50000 2  >>  data/bench180.dat
 java ParallelNbody 180 50000 3  >>  data/bench180.dat
 java ParallelNbody 180 50000 4  >>  data/bench180.dat
-java ParallelNbody 180 50000 5  >>  data/bench180.dat
-java ParallelNbody 180 50000 6  >>  data/bench180.dat
-java ParallelNbody 180 50000 7  >>  data/bench180.dat
-java ParallelNbody 180 50000 8  >>  data/bench180.dat
-java ParallelNbody 180 50000 9  >>  data/bench180.dat
-java ParallelNbody 180 50000 10  >>  data/bench180.dat
-java ParallelNbody 180 50000 11  >>  data/bench180.dat
-java ParallelNbody 180 50000 12  >>  data/bench180.dat
-java ParallelNbody 180 50000 13  >>  data/bench180.dat
-java ParallelNbody 180 50000 14  >>  data/bench180.dat
-java ParallelNbody 180 50000 15  >>  data/bench180.dat
-java ParallelNbody 180 50000 16  >>  data/bench180.dat
 
 
 
@@ -42,15 +18,3 @@ java ParallelNbody 240 50000 1  >  data/bench240.dat
 java ParallelNbody 240 50000 2  >>  data/bench240.dat
 java ParallelNbody 240 50000 3  >>  data/bench240.dat
 java ParallelNbody 240 50000 4  >>  data/bench240.dat
-java ParallelNbody 240 50000 5  >>  data/bench240.dat
-java ParallelNbody 240 50000 6  >>  data/bench240.dat
-java ParallelNbody 240 50000 7  >>  data/bench240.dat
-java ParallelNbody 240 50000 8  >>  data/bench240.dat
-java ParallelNbody 240 50000 9  >>  data/bench240.dat
-java ParallelNbody 240 50000 10  >>  data/bench240.dat
-java ParallelNbody 240 50000 11  >>  data/bench240.dat
-java ParallelNbody 240 50000 12  >>  data/bench240.dat
-java ParallelNbody 240 50000 13  >>  data/bench240.dat
-java ParallelNbody 240 50000 14  >>  data/bench240.dat
-java ParallelNbody 240 50000 15  >>  data/bench240.dat
-java ParallelNbody 240 50000 16  >>  data/bench240.dat
